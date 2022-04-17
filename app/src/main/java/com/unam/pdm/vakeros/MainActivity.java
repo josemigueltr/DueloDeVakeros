6d6520a4abea83354f77893f0c25165e3f88b02d
Omar Gramer <grameromar@ciencias.unam.mx>
2022-04-16 21:00:23 -0500

    Recreando pull request

--- app/src/main/java/com/unam/pdm/vakeros/MainActivity.java
+++ app/src/main/java/com/unam/pdm/vakeros/MainActivity.java
@@ -92,9 +92,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
+    public void onAccuracyChanged(Sensor sensor, int i) {}
 
     private void startTimer(){
         if(singleThreadProducer == null){
